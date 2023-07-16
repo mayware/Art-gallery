708226166ea62b137ae28a11d75d1a0fd36c3098
tofa93 <tofig.valiyev1993@gmail.com>
2023-07-17 00:42:06 +0200

    added few features

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,41 +8,69 @@ import catFive from '../assets/categories/8.jpg'
 import catSix from '../assets/categories/9.jpg'
 import catSeven from '../assets/categories/10.jpg'
 import catEight from '../assets/categories/11.jpg'
+import banner from "../assets/1.jpg";
 const Home = () => {
     return (
         <div className="content">
             <div className="home-content">
                 <div className="home-banner">
                     <div className="banner-images">
-                        <Carousel />
+                        {/* <Carousel /> */}
                     </div>
                 </div>
                 <div className="home-categories">
                     <div className="category-illustration">
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
                         <img src={catOne} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
-                        <img src={catTwo} className='category-img' alt="" />
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
+                        <img src={catFour} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
                         <img src={catThree} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
-                        <img src={catFour} className='category-img' alt="" />
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
+                        <img src={catTwo} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
                         <img src={catFive} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
                         <img src={catSix} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
                         <img src={catFour} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
                         <img src={catSeven} className='category-img' alt="" />
                     </div>
                     <div className="category-illustration">
+                        <div className="overlay">
+                            <span className="image-title">Image title</span>
+                        </div>
                         <img src={catEight} className='category-img' alt="" />
                     </div>
                 </div>
