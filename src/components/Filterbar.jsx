7d64a27ab081e598686fe6038716e16b78503a9c
tofa93 <tofig.valiyev1993@gmail.com>
2023-07-20 00:29:13 +0200

    added few more features to the filter buttons

--- src/components/Filterbar.jsx
+++ src/components/Filterbar.jsx
@@ -1,16 +1,45 @@
+import React, { useState } from 'react';
 import '../styles/filterbar.css';
 
 const Filterbar = () => {
+    const [activeButton, setActiveButton] = useState('All');
+
+    const handleButtonClick = (buttonName) => {
+        setActiveButton(buttonName);
+    };
+
     return (
         <div className="filter-bar">
             <div className="filter-buttons">
-                <button className="filter-btn" id='left-filter-btn'>All</button>
-                <button className="filter-btn">Oil paintings</button>
-                <button className="filter-btn">Lithographs</button>
-                <button className="filter-btn" id='right-filter-btn'>NFTS</button>
+                <button
+                    className={`filter-btn ${activeButton === 'All' ? 'active' : ''}`}
+                    onClick={() => handleButtonClick('All')}
+                    id="left-filter-btn"
+                >
+                    All
+                </button>
+                <button
+                    className={`filter-btn ${activeButton === 'Oil paintings' ? 'active' : ''}`}
+                    onClick={() => handleButtonClick('Oil paintings')}
+                >
+                    Oil paintings
+                </button>
+                <button
+                    className={`filter-btn ${activeButton === 'Lithographs' ? 'active' : ''}`}
+                    onClick={() => handleButtonClick('Lithographs')}
+                >
+                    Lithographs
+                </button>
+                <button
+                    className={`filter-btn ${activeButton === 'NFTS' ? 'active' : ''}`}
+                    onClick={() => handleButtonClick('NFTS')}
+                    id="right-filter-btn"
+                >
+                    NFTS
+                </button>
             </div>
         </div>
     );
-}
+};
 
 export default Filterbar;
