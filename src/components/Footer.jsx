ea4917f8e410049648c0e422197160b8453afdb6
tofa93 <tofig.valiyev1993@gmail.com>
2023-07-26 00:53:17 +0200

    added few small features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,14 @@ import '../styles/footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer-left"></div>
-            <div className="footer-right"></div>
+            <div className="footer-inner">
+                <div className="footer-left">
+                    <div className="footer-brand-name">
+                        <span className="footer-brand-name-title">Daghol</span>
+                    </div>
+                </div>
+                <div className="footer-right"></div>
+            </div>
         </div>
     );
 }
