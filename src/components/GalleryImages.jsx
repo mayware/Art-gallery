d5dfa65f9f0eb07937e54981f83509df2395a0e9
tofa93 <tofig.valiyev1993@gmail.com>
2023-07-29 22:01:17 +0200

    added few small features

--- src/components/GalleryImages.jsx
+++ src/components/GalleryImages.jsx
@@ -4,16 +4,16 @@ const GalleryImages = ({ images, openModal }) => {
     return (
         <div className="gallery-images">
             {images.map((image) => (
-                <div className="gallery-image-box" key={image.id}>
-                    <button className="gallery-image-pic" onClick={() => openModal(image)}>
+                <button className="gallery-image-box" onClick={() => openModal(image)} key={image.id}>
+                    <div className="gallery-image-pic">
                         <img className='gallery-image-img' src={image.image} alt="gallery-image" />
-                    </button>
+                    </div>
                     <div className="gallery-image-desc">
                         <span className="gallery-image-title">
                             {image.title}
                         </span>
                     </div>
-                </div>
+                </button>
             ))}
         </div>
     );
