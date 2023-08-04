c3a099e3503c6f3f29bed285b9e2a2218459cf9f
tofa93 <tofig.valiyev1993@gmail.com>
2023-08-04 12:43:33 +0200

    added new features

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,15 +9,26 @@ const Contact = () => {
     return (
         <div className="content">
             <div className="contact-content">
-                <div className="contact-illustration">
-                    <div className="contact-illustration-headline">
-                        <span className="contact-headline-text">
+                <div className="contact-left">
+                    <div className="contact-illustration">
+                        <div className="contact-illustration-headline">
+                            <span className="contact-headline-text">
 
-                        </span>
+                            </span>
+                        </div>
                     </div>
                 </div>
-                <div className="contact-form-box">
-                    <form action="POST" className="contact-form"></form>
+                <div className="contact-right">
+                    <div className="contact-form-box">
+                        <form action="POST" className="contact-form">
+                            <label htmlFor="name-field">Name</label>
+                            <input type="text" name="name-field" className="contact-name-field" required />
+                            <label htmlFor="email-field">Email</label>
+                            <input type="email" name="email-field" className="contact-email-field" required />
+                            <label htmlFor="textarea-field">Comment or Message</label>
+                            <textarea cols="30" rows="10" name="textarea-field" className="contact-textarea-field" required></textarea>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
