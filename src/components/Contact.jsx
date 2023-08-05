e91a5079898844a8f3f78d2ff5d1494f9922f5f9
tofa93 <tofig.valiyev1993@gmail.com>
2023-08-05 15:04:32 +0200

    added new stuff

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,12 +20,15 @@ const Contact = () => {
                 </div>
                 <div className="contact-right">
                     <div className="contact-form-box">
+                        <div className="contact-form-title">
+                            <div className="contact-form-title-text">Contact Me</div>
+                        </div>
                         <form action="POST" className="contact-form">
-                            <label htmlFor="name-field">Name</label>
-                            <input type="text" name="name-field" className="contact-name-field" required />
-                            <label htmlFor="email-field">Email</label>
-                            <input type="email" name="email-field" className="contact-email-field" required />
-                            <label htmlFor="textarea-field">Comment or Message</label>
+                            <label htmlFor="name-field" className="contact-field-label">Name</label>
+                            <input type="text" name="name-field" className="contact-input-field" required />
+                            <label htmlFor="email-field" className="contact-field-label">Email</label>
+                            <input type="email" name="email-field" className="contact-input-field" required />
+                            <label htmlFor="textarea-field" className="contact-field-label">Comment or Message</label>
                             <textarea cols="30" rows="10" name="textarea-field" className="contact-textarea-field" required></textarea>
                         </form>
                     </div>
