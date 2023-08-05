80a6e73903f2c83e61027e2d09c002513ea7a917
tofa93 <tofig.valiyev1993@gmail.com>
2023-08-05 16:33:25 +0200

    added few small changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,12 +25,12 @@ const Contact = () => {
                         </div>
                         <form action="POST" className="contact-form">
                             <label htmlFor="name-field" className="contact-field-label">Name</label>
-                            <input type="text" name="name-field" className="contact-input-field" required />
+                            <input type="text" name="name-field" className="contact-input-field" placeholder="Enter your full Name" required />
                             <label htmlFor="email-field" className="contact-field-label">Email</label>
-                            <input type="email" name="email-field" className="contact-input-field" required />
+                            <input type="email" name="email-field" className="contact-input-field" placeholder="Enter your Email" required />
                             <label htmlFor="textarea-field" className="contact-field-label">Comment or Message</label>
-                            <textarea cols="30" rows="10" name="textarea-field" className="contact-textarea-field" required></textarea>
-                            <input type="submit" className="contact-submit-btn" value="Send" />
+                            <textarea cols="30" rows="10" name="textarea-field" className="contact-textarea-field" placeholder="Write something.." required></textarea>
+                            <input type="submit" className="contact-submit-btn" value="SEND MESSAGE" />
                         </form>
                     </div>
                 </div>
